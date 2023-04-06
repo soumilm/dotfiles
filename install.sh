b75027b495ef85cc2d5d2743cd27552ae6f0ab50
Soumil Mukherjee <soumilm@stripe.com>
2023-04-06 07:52:40 -0700

    Install script useless

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run this once to make everything work
-echo "source ~/dotfiles/.bashrc" >> ~/.bashrc
-echo "source ~/dotfiles/.vimrc" >> ~/.vimrc
-echo "source-file ~/dotfiles/.tmux.conf" >> ~/.tmux.conf

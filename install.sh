5d9887f93754223cb4f62217d3b2c0b2c95ea6bd
Soumil Mukherjee <soumil0526@gmail.com>
2023-04-05 22:02:00 -0700

    Setup script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run this once to make everything work
+echo "source ~/dotfiles/.bashrc" >> ~/.bashrc
+echo "source ~/dotfiles/.vimrc" >> ~/.vimrc
+echo "source-file ~/dotfiles/.tmux.conf" >> ~/.tmux.conf

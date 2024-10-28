4001f30a31ca494189af501507ecd77eb3123b56
Soumil Mukherjee <soumilm@stripe.com>
2024-10-28 14:03:00 -0700

    Remove vundle

--- init.lua
+++ init.lua
@@ -1,12 +1,4 @@
 vim.cmd([[
-" set the runtime path to include Vundle and initialize
-set rtp+=~/.vim/bundle/Vundle.vim
-call vundle#begin()
-" let Vundle manage Vundle, required
-Plugin 'VundleVim/Vundle.vim'
-" Colorscheme plugin
-call vundle#end()            " required
-
 set rtp+=/opt/homebrew/opt/fzf
 
 "Marks Used

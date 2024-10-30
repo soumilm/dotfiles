e4d4efa8d5c57d08feaad0d93c187827539f1c75
Soumil Mukherjee <soumilm@stripe.com>
2024-10-29 17:13:33 -0700

    Remove redundant options

--- nvim/lua/plugins/spec.lua
+++ nvim/lua/plugins/spec.lua
@@ -1,27 +1,25 @@
 return {
   -- Try to keep this list ASCIIbetized
 
-  { "Raimondi/delimitMate", lazy = false },               -- automatically close quotes, parens, etc
-  { "SirVer/ultisnips", lazy = false },                   -- configurable tab-completed ultisnips
-  { "airblade/vim-gitgutter", lazy = false },             -- show diff icons on the left
-  { "christoomey/vim-tmux-navigator", lazy = false },     -- Ctrl+{h,j,k,l} navigates consistently across tmux and vim
+  { "Raimondi/delimitMate" },               -- automatically close quotes, parens, etc
+  { "SirVer/ultisnips" },                   -- configurable tab-completed ultisnips
+  { "airblade/vim-gitgutter" },             -- show diff icons on the left
+  { "christoomey/vim-tmux-navigator" },     -- Ctrl+{h,j,k,l} navigates consistently across tmux and vim
   {
-    "cocopon/iceberg.vim",                                -- theme
-    lazy = false,
+    "cocopon/iceberg.vim",                  -- theme
     priority = 1000,
     config = function()
       vim.cmd([[colorscheme "iceberg"]])
     end,
   },
   {
-    "fatih/vim-go",                                       -- golang miscellany
-    lazy = false,
+    "fatih/vim-go",                         -- golang miscellany
     ft = "go",
   },
-  { "google/vim-searchindex", lazy = false },             -- add count and index when searching
-  { "honza/vim-snippets", lazy = false },                 -- adds snippets for UltiSnips
+  { "google/vim-searchindex" },             -- add count and index when searching
+  { "honza/vim-snippets" },                 -- adds snippets for UltiSnips
   {
-    "ibhagwan/fzf-lua",                                   -- fuzzy file finder
+    "ibhagwan/fzf-lua",                     -- fuzzy file finder
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       "fzf-vim",
@@ -30,21 +28,21 @@ return {
       },
     },
   },
-  { "jparise/vim-graphql", lazy = false },                -- GraphQL syntax
-  { "leafgarland/typescript-vim", lazy = false },         -- TypeScript syntax
-  { "mattesgroeger/vim-bookmarks", lazy = false },        -- bookmarks for lines
-  { "maxmellon/vim-jsx-pretty", lazy = false },           -- JS and JSX syntax
-  { "mhinz/vim-startify", lazy = false },                 -- fancy start screen for vim
-  { "neovim/nvim-lspconfig", lazy = false },              -- LSP
+  { "jparise/vim-graphql" },                -- GraphQL syntax
+  { "leafgarland/typescript-vim" },         -- TypeScript syntax
+  { "mattesgroeger/vim-bookmarks" },        -- bookmarks for lines
+  { "maxmellon/vim-jsx-pretty" },           -- JS and JSX syntax
+  { "mhinz/vim-startify" },                 -- fancy start screen for vim
+  { "neovim/nvim-lspconfig" },              -- LSP
   {
-    "nvim-lualine/lualine.nvim",                          -- status line
+    "nvim-lualine/lualine.nvim",            -- status line
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {},
   },
-  { "sbdchd/neoformat", lazy = false },                   -- formatter
-  { "simnalamburt/vim-mundo", lazy = false },
-  { "tpope/vim-fugitive", lazy = false },                 -- git commands
-  { "tpope/vim-vinegar", lazy = false },                  -- file navigation
-  { "vim-python/python-syntax", lazy = false },           -- for f-strings
-  { "yegappan/mru", lazy = false },                       -- see most recently used files
+  { "sbdchd/neoformat" },                   -- formatter
+  { "simnalamburt/vim-mundo" },
+  { "tpope/vim-fugitive" },                 -- git commands
+  { "tpope/vim-vinegar" },                  -- file navigation
+  { "vim-python/python-syntax" },           -- for f-strings
+  { "yegappan/mru" },                       -- see most recently used files
 }

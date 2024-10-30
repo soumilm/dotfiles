70b3dab1f3c139dbbb75536f101195fbdb0d189e
Soumil Mukherjee <soumilm@stripe.com>
2024-10-29 18:25:20 -0700

    Autocomplete :D

--- nvim/lua/plugins/spec.lua
+++ nvim/lua/plugins/spec.lua
@@ -28,6 +28,88 @@ return {
       },
     },
   },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      local lspkind = require("lspkind")
+      local formatting = {
+        -- Taken from stevearc
+        format = lspkind.cmp_format({
+          mode = "symbol",
+          symbol_map = {
+            Copilot = " ",
+            Class = "󰆧 ",
+            Color = "󰏘 ",
+            Constant = "󰏿 ",
+            Constructor = " ",
+            Enum = " ",
+            EnumMember = " ",
+            Event = "",
+            Field = " ",
+            File = "󰈙 ",
+            Folder = "󰉋 ",
+            Function = "󰊕 ",
+            Interface = " ",
+            Keyword = "󰌋 ",
+            Method = "󰊕 ",
+            Module = " ",
+            Operator = "󰆕 ",
+            Property = " ",
+            Reference = "󰈇 ",
+            Snippet = " ",
+            Struct = "󰆼 ",
+            Text = "󰉿 ",
+            TypeParameter = "󰉿 ",
+            Unit = "󰑭",
+            Value = "󰎠 ",
+            Variable = "󰀫 ",
+          },
+          menu = {
+            buffer = "[buf]",
+            nvim_lsp = "[LSP]",
+            nvim_lua = "[api]",
+            path = "[path]",
+            luasnip = "[snip]",
+          },
+        }),
+      }
+
+      cmp.setup({
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+          { name = "buffer" },
+        },
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        mapping = {
+          ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<CR>"] = cmp.mapping(function()
+            cmp.confirm( {select=true} )
+          end)
+        },
+      })
+    end,
+    dependencies = {
+      -- Autocompletion
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "onsails/lspkind.nvim",
+
+      -- Snippets
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
   { "jparise/vim-graphql" },                -- GraphQL syntax
   { "leafgarland/typescript-vim" },         -- TypeScript syntax
   { "mattesgroeger/vim-bookmarks" },        -- bookmarks for lines
